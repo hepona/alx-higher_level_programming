3f1a011f5a84f302be769d221ab0251abfdf41a5
hepona <hepona10@gmail.com>
2023-06-06 17:12:46 -0700

    pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a * (a * b)
+    r = 1
+    if b == 0:
+        return 1
+    elif b < 0:
+        b = b * -1
+        for i in range(b):
+            r = r * a
+        return 1 / r
+    else:
+        for i in range(b):
+            r = r * a
+        return r
