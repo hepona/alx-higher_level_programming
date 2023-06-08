8626ddac7823ed43330e5824207df1bc8bbf64ea
hepona <hepona10@gmail.com>
2023-06-08 10:39:33 -0700

    argv tst

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if ln == 1:
         print("0 arguments.")
     elif ln == 2:
-        print("1 argument: ")
+        print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments: ".format(ln - 1))
+        print("{} arguments:".format(ln - 1))
         for i in range(1, ln):
             print("{}: {}".format(i, argv[i]))
