e8db1b8918c5e0a471d4051ea00a5208efea422c
hepona <hepona10@gmail.com>
2023-06-08 10:25:23 -0700

    arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif ln == 2:
         print("{} argument: ".format(ln - 1))
-        print("{}: {}".format(ln - 1,  argv[ln - 1]))
+        print("{}: {}".format(ln - 1,  argv[1]))
     else:
         print("{} arguments: ".format(ln - 1))
         for i in range(1, ln):
