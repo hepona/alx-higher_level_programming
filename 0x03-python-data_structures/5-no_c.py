901e05e57c018f4187b0625ef050df17a228be65
hepona <hepona10@gmail.com>
2023-06-10 14:06:26 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     nw_string = ""
     for i in range(0, len(my_string)):
-        if my_string[i] != 'c' and  my_string[i] != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             nw_string = nw_string + my_string[i]
     return nw_string
