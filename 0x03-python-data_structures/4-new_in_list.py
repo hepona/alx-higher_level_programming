c7299efceb67ff2acbd5ae26184b09da5f976a8a
hepona <hepona10@gmail.com>
2023-06-10 13:40:41 -0700

    replace in cpy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     my_list_cp = my_list.copy()
+    if my_list is None:
+        return None
     if idx < 0 and idx >= len(my_list):
         return my_list_cp
     else:
