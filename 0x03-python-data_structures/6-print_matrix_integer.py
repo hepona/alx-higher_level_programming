b97745fdf5960b2cd77abd2be6395d01481b5af4
hepona <hepona10@gmail.com>
2023-06-10 18:02:41 -0700

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1:
+    if len(matrix) == 1 or matrix is None:
         return print("")
     for i in range(len(matrix)):
         for j in range(len(matrix)):
