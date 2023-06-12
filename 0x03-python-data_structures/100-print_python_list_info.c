de54f5b6d5fea07b0307f94e9162596181ed5012
hepona <hepona10@gmail.com>
2023-06-12 19:38:42 +0000

    done

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,15 +5,14 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t ln = PyList_Size(p);
 	Py_ssize_t alloced = PyList_GET_SIZE(p);
 	Py_ssize_t i;
-	PyObject *item;
+	PyTypeObject *itemType;
 
 	printf("[*] Size of the Python List = %zd\n", ln);
 	printf("[*] Allocated = %zd\n", alloced);
 	for (i = 0 ; i < ln ; i++)
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %zd: \n", i);
-		PyObject_Print(item, stdout, Py_PRINT_RAW);
+		itemType = Py_TYPE(PyList_GetItem(p, i));
+		printf("Element %zd: %s", i, itemType->tp_name);
 		printf("\n");
 	}
 }	
