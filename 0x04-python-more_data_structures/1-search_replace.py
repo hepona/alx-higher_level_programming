2ea32bbd13fb47a70dd60d821c2d1043ccf07e6f
hepona <hepona10@gmail.com>
2023-06-14 10:51:25 -0700

    done

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) == 0:
+        return my_list
+    my_list_c = my_list.copy()
+    my_list_c[search-1] = replace
+    return my_list_c
