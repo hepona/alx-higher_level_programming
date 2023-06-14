ffcdbd0aa5dbb721c85555e7447e3577217f35c5
hepona <hepona10@gmail.com>
2023-06-14 11:25:02 -0700

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
     if search > len(my_list) or search <= 0:
         return my_list
     my_list_c = my_list.copy()
-    my_list_c[search-1] = replace
+    my_list_c[search - 1] = replace
     return my_list_c
