9f50fc0e5a3d2b1ed744a97b2afd53ca085e1cea
hepona <hepona10@gmail.com>
2023-06-15 04:11:52 -0700

    done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,9 @@ def weight_average(my_list=[]):
     if my_list is not my_list :
         return 0
     s = 0
+    q = 0
     for i in my_list:
-        for j in my_list:
-            s = s + (i * j)
-            q = j + j
+        s = s + (i[0] * i[1])
+        q = q + i[1]
     r = s / q
     return r
