118ff5975ffeb11e41df9b3f7072d8c238fc1879
hepona <hepona10@gmail.com>
2023-06-14 17:56:42 -0700

    done

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if len(a_dictionary) == 0:
-        return a_dictionary
+        return ""
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
