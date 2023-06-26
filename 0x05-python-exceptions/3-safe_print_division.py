604d09ff714d2a280c1968eb21284646f22da323
hepona <hepona10@gmail.com>
2023-06-26 13:09:57 -0700

    3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    q = 0
+    try:
+        q = a / b
+    except:
+        q = None
+    finally:
+        print("Inside result: {}".format(q))
+        return q
