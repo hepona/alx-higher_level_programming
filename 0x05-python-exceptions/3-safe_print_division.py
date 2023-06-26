dc8f50faa56f34aab2785bcde5b826073c1611f1
hepona <hepona10@gmail.com>
2023-06-26 13:12:08 -0700

    3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     q = 0
     try:
         q = a / b
-    except(ValueError):
+    except(ValueError, IndexError):
         q = None
     finally:
         print("Inside result: {}".format(q))
