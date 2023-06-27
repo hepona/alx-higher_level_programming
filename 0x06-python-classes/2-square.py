da60ad371bfdd84a1494d710267a7e997d414e2b
hepona <hepona10@gmail.com>
2023-06-27 07:17:22 -0700

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """define class attribute"""
 
-    def __init__(self, __size=0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
