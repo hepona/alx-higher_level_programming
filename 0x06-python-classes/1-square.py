d38d8ea2507691684a9237b66fbd608238febfa6
hepona <hepona10@gmail.com>
2023-06-27 05:47:58 -0700

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Define class square"""
+
+
 class Square:
     """define attribute of square"""
-    def __init__(self,__size="0"):
+    def __init__(self, __size="0"):
         self.__size = __size
