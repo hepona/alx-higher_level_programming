e017e4bf9662473055c53eeee14e68b0c65fb528
hepona <hepona10@gmail.com>
2023-06-28 05:57:49 -0700

    6?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,20 @@ class Square:
     """define class attribute"""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        if (
+            len(position) != 2
+            or not isinstance(position, int)
+            or not position >= 0
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
 
     @property
     def size(self):
