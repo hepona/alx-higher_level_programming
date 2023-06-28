05e09ad48858bff8ee8b17fa8dfea0ac5c92244c
hepona <hepona10@gmail.com>
2023-06-27 18:04:37 -0700

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,6 +45,6 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        for i in range(self.__position[1], self.__size + self.__position[1]):
+        for i in range(self.__size):
             print(" " * self.__position[0], end="")
             print("#" * self.__size)
