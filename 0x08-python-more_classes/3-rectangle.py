7566006de3817ff5f78b7c5d4d15efeed1caa382
hepona <hepona10@gmail.com>
2023-07-03 10:37:06 -0700

    3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = width
-        if isinstance(height, int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
