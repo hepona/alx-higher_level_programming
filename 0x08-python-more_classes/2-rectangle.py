0332aa5aacc667d797590aee670a6d103f204adb
hepona <hepona10@gmail.com>
2023-07-03 07:15:18 -0700

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,13 +8,13 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if width < 0 or width is None:
             raise ValueError("width must be >= 0")
         else:
             self.__width = width
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if height < 0 or height is None:
             raise ValueError("height must be >= 0")
         else:
             self.__height = height
@@ -27,7 +27,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value < 0 or value is None:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -40,7 +40,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value < 0 or value is None:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
