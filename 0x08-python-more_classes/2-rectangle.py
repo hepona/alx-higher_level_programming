091e79d41fc15f4d3e51b348de987b4b4f939573
hepona <hepona10@gmail.com>
2023-07-03 09:35:32 -0700

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,13 +6,13 @@ class Rectangle:
     """define it's attribut"""
 
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int) or width is None:
+        if width is None or not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = width
-        if not isinstance(height, int) or height is None:
+        if height is None or not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
@@ -25,7 +25,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int) or value is None:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -38,7 +38,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int) or value is None:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
