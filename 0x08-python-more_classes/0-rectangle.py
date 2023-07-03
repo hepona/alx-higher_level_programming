9ef8c7676796d48a5195e0caa853b28918eec652
hepona <hepona10@gmail.com>
2023-07-03 06:38:30 -0700

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """define a class rectangle"""
+
+
 class Rectangle:
-    """ define rectangle's attribute """
+    """define rectangle's attribute"""
+
     def __init__(self):
         pass
