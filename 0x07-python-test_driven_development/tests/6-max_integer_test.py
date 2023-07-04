d7d3f260f85669c483cc031c4bd40516e15934b4
Rahif hiba <57334931+hepona@users.noreply.github.com>
2023-07-04 23:11:35 +0100

    tdd

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+class TestMaxInteger(unittest.TestCase):
+    def test_maxinteger(self):
+        # test max int
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, -2, 7, 4]), 7)
+        self.assertAlmostEqual(max_integer([0.1, 2.79, 3.666, -10.9]), 3.666)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([]), None)
+        
+
