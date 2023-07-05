6887607828c71fcc3a4640bae9cd174c4be84704
Rahif hiba <57334931+hepona@users.noreply.github.com>
2023-07-05 14:06:13 +0100

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,11 +5,13 @@ This is a simple module that print text with 2 new lines
 
 
 def text_indentation(text):
-    """function that print text with 2 new lines """
+    """function that print text with 2 new lines"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    text = text.replace(" .", ".").replace(" ?", "?").replace(" :", ":")
+    text = text.replace(". ", ".").replace("? ", "?").replace(": ", ":")
     for i in text:
         print("{}".format(i), end="")
         if i in [".", "?", ":"]:
-            print("\n")
+            print("\n", end="")
     print()
