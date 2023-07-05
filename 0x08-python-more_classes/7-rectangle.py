28d77bd326ae0fadfa780ddc035be2b77918bd44
Rahif hiba <57334931+hepona@users.noreply.github.com>
2023-07-05 14:45:12 +0100

    symb

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """define it's attribut"""
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = ""
 
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
