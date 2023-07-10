d07b6d233845090585dc2f6ba211fa4c7d369c95
hepona <hepona10@gmail.com>
2023-07-10 11:23:13 -0700

    1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """define MyList class"""
 
+
 class MyList(list):
     """define it attribute"""
+
     def print_sorted(self):
         self_cp = self.copy()
         self_cp.sort()
-        print (self_cp)
+        print(self_cp)
