9a58e07fcf46de2030639b0cdec9249fed070076
hepona <hepona10@gmail.com>
2023-07-10 15:10:17 -0700

    6

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define BaseGeometry class"""
+
+
+class BaseGeometry:
+    """define it's attribute"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(name,"name must be an integer")
+        if value <= 0:
+            raise ValueError(name,"name must be greater than 0")
