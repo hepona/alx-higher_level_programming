2457d56d6bd0d1ec5d277976bf3c9cc7de6991b4
hepona <hepona10@gmail.com>
2023-07-10 14:54:13 -0700

    5

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define BaseGeometry class"""
+
+
+class BaseGeometry:
+    """define it's attribute"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
