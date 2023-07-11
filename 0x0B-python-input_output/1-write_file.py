808c7f8693f54f6b80fe3b9d5ad5e6ced0086291
hepona <hepona10@gmail.com>
2023-07-11 05:49:10 -0700

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that return num of characters"""
+
+
+def write_file(filename="", text=""):
+    """return num of character"""
+    with open(filename, mode="w",  encoding="utf-8") as myfile:
+        myfile.write(text)
+    with open(filename, encoding="utf-8") as myfile:
+        char_num = 0
+        while 1:
+            charead = myfile.read(1)
+            char_num = char_num + 1
+            if not charead:
+                break
+    return char_num
