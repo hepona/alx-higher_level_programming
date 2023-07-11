19d3f285df827970ceee41dd7d963fcdb8d03f65
hepona <hepona10@gmail.com>
2023-07-11 08:33:48 -0700

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """function that append string at the end of a text file"""
+
+
 def append_write(filename="", text=""):
     """append txt at the end of a file"""
-    with open(filename, encode = "utf-8", mode = "a") as file:
-
+    with open(filename, encoding = "utf-8", mode = "a") as file:
+        file.write(text)
+    with open(filename, encoding = "utf-8", mode = "r") as file:
+        num_char = len(file.read())
+    return num_char
