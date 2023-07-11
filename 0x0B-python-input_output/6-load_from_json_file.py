02e90a154a490fdb3f31ad150a6be4eafdabe746
hepona <hepona10@gmail.com>
2023-07-11 15:32:15 -0700

    6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,7 +2,8 @@
 """define function that creates an Object from a “JSON file"""
 
 
+import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file"""
-    with open(filename, encoder="utc-8", mode="w") as file:
-        json.load(file)
+    with open(filename, encoding = "utf-8", mode = "r") as file:
+        json.dumps(file.read())
