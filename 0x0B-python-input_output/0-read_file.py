d4488ac4792f6401e5253bca375f8000cacddaa5
hepona <hepona10@gmail.com>
2023-07-11 04:58:46 -0700

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """read a file"""
     with open(filename, mode="r",  encoding="utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
