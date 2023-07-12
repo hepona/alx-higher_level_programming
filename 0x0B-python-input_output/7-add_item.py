ed21495a29c08f2cf3f1e284584a381a98bb6003
hepona <hepona10@gmail.com>
2023-07-11 18:11:51 -0700

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list, and save them in a file"""
+"""script that adds all arguments to a list"""
 if __name__ == "__main__":
     """script that adds all arguments to a Python list"""
     from sys import argv
 
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    load_from_json = __import__("6-load_from_json_file").load_from_json_file
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
     filename = "add_item.json"
     for i in range(1, len(argv)):
         save_to_json_file(str(argv[i]), filename)
-    print(load_from_json_file(filename))
+    print(load_from_json(filename))
