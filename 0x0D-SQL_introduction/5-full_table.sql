d523b8dc23de58dec0ddd2c8a8ac795aaa012235
hepona <hepona10@gmail.com>
2023-10-17 18:07:55 +0100

    6

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+SELECT *
 FROM  information_schema.COLUMNS
 WHERE table_name= 'first_table'
