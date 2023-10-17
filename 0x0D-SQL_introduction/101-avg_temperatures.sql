5fa08e54581163a76535b0a330eea33fdfd314c2
hepona <hepona10@gmail.com>
2023-10-17 21:00:06 +0100

    18

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVG(value) as avg_temp FROM temperatures
+GROUP BY city, value ORDER BY value DESC
