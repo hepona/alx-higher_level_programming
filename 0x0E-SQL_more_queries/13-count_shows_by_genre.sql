5c6dd7bf5a1548f656c284925e6c73c53b3662c1
hepona <hepona10@gmail.com>
2023-10-19 00:44:54 +0100

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,7 +5,8 @@ SELECT
 FROM
     tv_genres
     LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name IS NOT NULL
 GROUP BY
     tv_genres.name
 ORDER BY
-    number_of_shows DESC
+    number_of_shows DESC;
