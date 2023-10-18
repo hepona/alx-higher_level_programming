7289711f23056a225b816da8ee8caa924d0d92e0
hepona <hepona10@gmail.com>
2023-10-19 00:47:05 +0100

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,7 +5,7 @@ SELECT
 FROM
     tv_genres
     LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name IS NOT NULL
+WHERE tv_show_genres.show_id IS NOT NULL
 GROUP BY
     tv_genres.name
 ORDER BY
