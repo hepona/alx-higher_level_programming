5d612d40483646a5191808d08249bf61d57cb0ea
hepona <hepona10@gmail.com>
2023-10-18 12:19:27 +0100

    1 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT * on hbtn_0c_0 TO 'user_0d_2'@'localhost' ;
+GRANT SELECT, INSERT, UPDATE, DELETE on hbtn_0c_0.* TO 'user_0d_2'@'localhost' ;
