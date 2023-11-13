65f077eed1a815f31bb36622343e52a1b149b597
hepona <hepona10@gmail.com>
2023-11-13 22:10:39 +0100

    3 4 5

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const arg = process.argv;
+if (arg === 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const arg = process.argv;
+if (isNaN(parseInt(arg[2])) === true) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ', parseInt(arg[2]));
+}
