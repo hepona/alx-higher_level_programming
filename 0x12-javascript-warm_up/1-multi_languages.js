6d59ac84dcdd7be616003038211274919acdc2f5
hepona <hepona10@gmail.com>
2023-11-13 13:04:25 +0100

    0 1 2

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,3 +3,4 @@ const t = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 console.log(t[0]);
 console.log(t[1]);
 console.log(t[2]);
+//
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,3 +7,4 @@ if (arg === 2) {
 } else {
   console.log('Arguments found');
 }
+//z
