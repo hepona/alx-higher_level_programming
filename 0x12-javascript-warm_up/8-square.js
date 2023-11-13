9adfd5cd298bc9802733dc5540623088499d8da7
hepona <hepona10@gmail.com>
2023-11-14 00:56:28 +0100

    7 8 9

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const n = process.argv[2];
+if (isNaN(n)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < n; i++) {
+    let x = '';
+    for (let j = 0; j < n; j++) {
+      x = x + 'X';
+    }
+    console.log(x);
+  }
+}
