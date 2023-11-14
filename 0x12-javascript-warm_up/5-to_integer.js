5e97bf60eef32261b80d557d9d364376e5d7f491
hepona <hepona10@gmail.com>
2023-11-14 14:41:02 +0100

    fix task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const arg = process.argv[2];
 if (Number.parseInt(arg) !== Number(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', parseInt(arg));
+  console.log('My number:', parseInt(arg));
 }
