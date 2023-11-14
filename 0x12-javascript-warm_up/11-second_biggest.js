9b79af7c024854249f1be85ebd8db9bd1c1ee52d
hepona <hepona10@gmail.com>
2023-11-14 02:29:23 +0100

    11 13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 const t = process.argv.slice(2);
-if (t.length === 0) {
+if (t.length === 0 || t.length === 1) {
   console.log('0');
-} else if (t.length === 1) {
-  console.log('1');
 } else {
   console.log(Math.max(...t.map(Number)) - 1);
 }
