6370e27e4bda1dd616d06090e81d3c88d9e2d11f
hepona <hepona10@gmail.com>
2023-11-14 01:17:42 +0100

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arg = process.argv;
-if (isNaN(arg[2])) {
+if (isNaN(arg[2]) === true) {
   console.log('Not a number');
 } else {
   console.log('My number: ', parseInt(arg[2]));
