72e263f8dbcde1c674b2a69de6dca55200cc2e85
hepona <hepona10@gmail.com>
2023-11-14 19:20:07 +0100

    7 8 9

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const listCp = [];
+  for (let i = 0; i < list.length; i++) {
+    listCp[i] = list[(list.length - 1) - i];
+  }
+  return listCp;
+};
