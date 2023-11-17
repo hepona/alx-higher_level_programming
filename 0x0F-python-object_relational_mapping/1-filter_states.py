b75b3344e7528470f52d2e8c145f36cf46558700
hepona <hepona10@gmail.com>
2023-11-17 22:17:11 +0100

    1 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""lists all states with a name starting with N (upper N)
+ from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_host = "localhost"
+    db_user = sys.argv[1]
+    db_passwd = sys.argv[2]
+    db = sys.argv[3]
+    port = 3306
+    db = MySQLdb.connect(
+        host=db_host,
+        port=port,
+        user=db_user,
+        passwd=db_passwd,
+        db=db
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    result = cursor.fetchall()
+    for row in result:
+        print("({}, {})".format(row[0], row[1]))
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument."""
+import sys
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_host = "localhost"
+    db_user = sys.argv[1]
+    db_passwd = sys.argv[2]
+    db = sys.argv[3]
+    port = 3306
+    db = MySQLdb.connect(
+        host=db_host,
+        port=port,
+        user=db_user,
+        passwd=db_passwd,
+        db=db
+    )
+    cursor = db.cursor()
+    q = "SELECT * FROM states WHERE name = '" + sys.argv[4] + "' ORDER BY id"
+    cursor.execute(q)
+    r = cursor.fetchall()
+    for row in r:
+        print("({}, '{}')".format(row[0], row[1]))
+    cursor.close()
+    db.close()
