8814567024a1606a123ffec946ccc9750c31f319
hepona <hepona10@gmail.com>
2023-11-18 22:17:32 +0100

    7 8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, MetaData, create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        r = connection.execute(text("SELECT * FROM states ORDER BY id"))
+        for i in r:
+            print("{}: {}".format(i[0], i[1]))
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
+    with engine.connect() as cn:
+        r = cn.execute(text("SELECT * FROM states WHERE id = 1 ORDER BY id"))
+        for row in r:
+            print("{}: {}".format(row[0], row[1]))
