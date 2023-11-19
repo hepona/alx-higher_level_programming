250ab0a7d89125cadd63842dd0f9c9e529f174be
hepona <hepona10@gmail.com>
2023-11-19 02:46:53 +0100

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
-        )
+        ), pool_pre_pint=True
     )
     Session = sessionmaker()
     session = Session(bind=engine)
