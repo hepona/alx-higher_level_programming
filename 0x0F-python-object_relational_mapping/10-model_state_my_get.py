85878ea23f921a535bd2a9618e58b4084cb92858
hepona <hepona10@gmail.com>
2023-11-19 02:27:00 +0100

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+"""prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    name = sys.argv[4]
+    state = session.query(State).filter_by(name=name).order_by(State.id).all()
+    if not state:
+        print("Not found")
+    for s in state:
+        print(s.id)
+    session.close()
