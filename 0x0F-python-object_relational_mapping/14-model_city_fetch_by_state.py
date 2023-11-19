85c9c16110b801f2a16f83ea0e140ac375754363
hepona <hepona10@gmail.com>
2023-11-19 20:48:25 +0100

    14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""prints all City objects from the database hbtn_0e_14_usa"""
+from sys import argv
+from sqlalchemy import create_engine, update
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_city import Base, City
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    r = session.query(City, State).order_by(City.id)
+    for i in r:
+        print("{}: ({}) {}".format(i.State.name, i.City.id, i.City.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Definition of class City"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """define it's attribute"""
+
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"))
