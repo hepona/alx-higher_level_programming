917d46a4d2c9d06ccc3fbf8bf67db5990b56d4f3
hepona <hepona10@gmail.com>
2023-11-19 15:54:20 +0100

    11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import create_engine, insert
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    nw = State(name="Louisiana")
+    session.add(nw)
+    session.commit()
+    print("{}".format(nw.id))
