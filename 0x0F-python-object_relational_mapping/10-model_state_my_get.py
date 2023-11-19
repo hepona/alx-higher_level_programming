286e53c280ee86e733b739cc3641ea3d84e4e09d
hepona <hepona10@gmail.com>
2023-11-19 15:10:47 +0100

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
+"""Start link class to table in database
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-"""prints the State object with the name passed as
-argument from the database hbtn_0e_6_usa"""
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
-        pool_pre_ping=True
+        pool_pre_ping=True,
     )
-    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    s = session.query(State).order_by(State.id).all()
-    found = 0
-    for state in s:
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = 1
-    if found == 0:
+
+    result = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if result:
+        print("{}".format(result.id))
+    else:
         print("Not found")
-    session.close()
