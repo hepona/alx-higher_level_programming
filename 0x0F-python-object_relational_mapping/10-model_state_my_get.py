e6adc26eb7dc8985722e8d642856790cd4d2ad5c
hepona <hepona10@gmail.com>
2023-11-19 14:50:18 +0100

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 """prints the State object with the name passed as
 argument from the database hbtn_0e_6_usa"""
