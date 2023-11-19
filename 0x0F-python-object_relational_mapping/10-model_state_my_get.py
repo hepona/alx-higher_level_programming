ac114bd66f3cdce18a9566c95bd6db85dd1cdf1b
hepona <hepona10@gmail.com>
2023-11-19 14:26:45 +0100

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""List all states"""
+from sys import argv
 from model_state import Base, State
-
-"""prints the State object with the name passed as
-argument from the database hbtn_0e_6_usa"""
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = (
-        session.query(State).where(State.name == sys.argv[4]).order_by(State.id).first()
-    )
-    if not state:
-        print("Not found")
+    states = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
     else:
-        print("{}".format(state.id))
+        print("Not found")
     session.close()
