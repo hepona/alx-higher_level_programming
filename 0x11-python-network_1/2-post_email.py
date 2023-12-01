d4ef24a2a9753456f611fb234da3d7be7f67fd9e
hepona <hepona10@gmail.com>
2023-12-01 20:17:08 +0100

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""takes in a URL and an email, sends a POST request to the passed
+ URL with the email as a parameter, and displays the body of the response"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": argv[2]})
+    data = data.encode("ascii")
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
