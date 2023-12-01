1fbb39c113257855adbe826e9b09a583e22b9ff1
hepona <hepona10@gmail.com>
2023-12-01 22:00:53 +0100

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+import requests
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    if response.status_code == 200:
+        print("Body response:")
+        print(f"\t- type: {type(response.text)}")
+        print(f"\t- content: {response.text}")
