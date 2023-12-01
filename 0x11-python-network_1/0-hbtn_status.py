7fa14d8c3f774bdf9bf72930e661263e438155ed
hepona <hepona10@gmail.com>
2023-12-01 17:21:33 +0100

    0 fetch

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import urllib.request
-
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")\
                 as response:
