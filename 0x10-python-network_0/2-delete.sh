d74c5266b8b1228b2f51c2baa889f7fb0ef76480
hepona <hepona10@gmail.com>
2023-12-01 01:14:41 +0100

    2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -s -X DELETE "$1"; curl -s "%{http_code}" -o /bin/null
