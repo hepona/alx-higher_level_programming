c5bc7bea22fef44f209809cd1088e4d5f8cf55aa
hepona <hepona10@gmail.com>
2023-12-01 04:05:37 +0100

    6 fix output wip

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,19 +5,20 @@
 def find_peak(list_of_integers):
     """function that finds a peak in a list of unsorted integers."""
     if len(list_of_integers) == 0:
-            return "None"
+        return "None"
     res = []
     for i in range(len(list_of_integers)):
         if i == 0:
-            if list_of_integers[i] > list_of_integers[i + 1]:
+            if list_of_integers[i] >= list_of_integers[i + 1]:
                 res.append(list_of_integers[i])
         elif i == len(list_of_integers) - 1:
-            if list_of_integers[i] > list_of_integers[i - 1]:
+            if list_of_integers[i] >= list_of_integers[i - 1]:
                 res.append(list_of_integers[i])
         else:
             if (
-                list_of_integers[i] > list_of_integers[i + 1]
-                and list_of_integers[i] > list_of_integers[i - 1]
+                list_of_integers[i] >= list_of_integers[i + 1]
+                and list_of_integers[i] >= list_of_integers[i - 1]
             ):
                 res.append(list_of_integers[i])
-    return str(res).strip('[]')
+    mx = max(res)
+    return mx
