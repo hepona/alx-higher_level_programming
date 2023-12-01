b61d866aacbc896800f86c2d5360c1332922c3a7
hepona <hepona10@gmail.com>
2023-12-01 17:18:46 +0100

    0 fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        r = response.read()
+        print("Body response:")
+        print("\t- type: ", type(r))
+        print("\t- content: ", r)
+        print("\t- utf8 content: ", r.decode("UTF-8"))
