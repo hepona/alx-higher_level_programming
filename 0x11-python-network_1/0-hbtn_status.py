a8ef03574258d474fcaee7a43916dbf070f4fffe
hepona <hepona10@gmail.com>
2023-12-01 17:20:27 +0100

    0 fetch

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")\
+                as response:
         r = response.read()
         print("Body response:")
         print("\t- type: ", type(r))
