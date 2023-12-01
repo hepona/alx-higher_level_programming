8d156c0e85dba7e79d0276883d490393b29ae197
hepona <hepona10@gmail.com>
2023-12-01 03:50:03 +0100

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers."""
+    res = []
+    for i in range(len(list_of_integers)):
+        if len(list_of_integers) == 0:
+            return None
+        elif i == 0:
+            if list_of_integers[i] > list_of_integers[i + 1]:
+                res.append(list_of_integers[i])
+        elif i == len(list_of_integers) - 1:
+            if list_of_integers[i] > list_of_integers[i - 1]:
+                res.append(list_of_integers[i])
+        else:
+            if (
+                list_of_integers[i] > list_of_integers[i + 1]
+                and list_of_integers[i] > list_of_integers[i - 1]
+            ):
+                res.append(list_of_integers[i])
+    return res
