607fd45eae9119951e0bf651c1d6e207e3d53a3b
hepona <hepona10@gmail.com>
2023-12-02 01:29:25 +0100

    9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Python script that takes your GitHub credentials
+ and uses the GitHub API to display your id"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get("https://api.github.com/user",read:user,
+     {"username": argv[1], "password": argv[2]})
+    if r.status_code == 200:
+        print("succed",r.headers.get("id"))
