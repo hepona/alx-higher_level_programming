c5618971d68464118f18511b70d3a2de89cc42a0
hepona <hepona10@gmail.com>
2023-12-02 01:31:04 +0100

    9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get("https://api.github.com/user",read:user,
-     {"username": argv[1], "password": argv[2]})
+    basic = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get("https://api.github.com/user",auth=basic)
     if r.status_code == 200:
         print("succed",r.headers.get("id"))
