5e7d228c854fc61bcfbb8483c9055cde43fdf6a0
hepona <hepona10@gmail.com>
2024-02-20 16:47:35 +0100

    4+5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  else {
+    const fs = require('fs');
+    fs.writeFile(file, body, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
