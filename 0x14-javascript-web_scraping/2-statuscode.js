602368c159ef95047e92dcf9bdd68ea65de85f1b
hepona <hepona10@gmail.com>
2024-02-20 13:57:10 +0100

    2+3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('code:', res.statusCode);
+});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const idmovie = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + idmovie;
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
