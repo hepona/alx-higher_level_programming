2d541c354347bc3e11578530da1147df0f3d76af
hepona <hepona10@gmail.com>
2024-02-20 16:40:35 +0100

    333

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  else {
+    let count = 0;
+    const ch = JSON.parse(body).results;
+    for (let i = 0; i < ch.length; i++) {
+      console.log(ch[i].characters);
+      if ((ch[i].characters).includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
