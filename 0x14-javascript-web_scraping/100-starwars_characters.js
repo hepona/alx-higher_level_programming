b9b3e4eb315d8aa992e1e4365867171fa73a2175
Rahif hiba <57334931+hepona@users.noreply.github.com>
2024-02-21 03:46:26 +0100

    100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const request = require('request');
+const idfilm = process.argv[2];
+const urlfilm = 'https://swapi-api.alx-tools.com/api/films/' + idfilm;
+const urlchara = 'https://swapi-api.alx-tools.com/api/people/';
+request(urlfilm, (err, res, body) => {
+  if (err) console.error(err);
+  else {
+    // let count = 0;
+    const idlst = [];
+    const ch = JSON.parse(body).characters;
+    for (const i in ch) {
+      const idchara = ch[i].split('/').slice(-2, -1)[0];
+      idlst.push(idchara);
+    }
+    for (const i in idlst) {
+      request(urlchara + i, (err, res, body) => {
+        if (err) console.error(err);
+        else console.log(JSON.parse(body).name);
+      });
+    }
+  }
+});
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+console.log(url)
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  for (const i of body) {
+    console.log(body[i])
+  }
+
+  })
